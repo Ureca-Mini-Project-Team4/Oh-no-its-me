a84bc874ed4a2526bc1ecb6285894ce62703db3d
arty0928 <arty0928@konkuk.ac.kr>
2025-03-17 15:08:30 +0900

    FEAT: #10 CommentException, CommentServiceImp get 작성

---
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/dto/CommentException.java
@@ -0,0 +1,7 @@
+package com.uplus.eureka.comment.model.dto;
+
+public class CommentException {
+//    public CommentException(String msg) {
+//        super(msg);
+//    }
+}
---
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/service/CommentServiceImp.java
@@ -0,0 +1,23 @@
+package com.uplus.eureka.comment.model.service;
+
+import com.uplus.eureka.comment.model.dao.CommentDao;
+import com.uplus.eureka.comment.model.dto.Comment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImp implements CommentService {
+
+    private final CommentDao dao;
+
+    public CommentServiceImp(CommentDao dao) {
+        if(dao ==null){
+            throw new IllegalArgumentException("dao is null");
+        }
+        this.dao = dao;
+    }
+
+    @Override
+    public Comment get(String commentId) {
+        return null;
+    }
+}
