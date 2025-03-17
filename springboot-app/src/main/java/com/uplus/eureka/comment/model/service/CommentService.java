3639a0557cd4b1b95c63767d0f8b5062918f6ecd
arty0928 <arty0928@konkuk.ac.kr>
2025-03-17 14:50:23 +0900

    FEAT: #10 CommentService get 인터페이스 추가

---
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.uplus.eureka.comment.model.service;
+
+import com.uplus.eureka.comment.model.dto.Comment;
+import com.uplus.eureka.user.model.dto.User;
+
+public interface CommentService {
+//    User login(String userId, String pass);
+//    User get(String userId);
+//    void signup(User user);
+
+    Comment get(String commentId);
+}
