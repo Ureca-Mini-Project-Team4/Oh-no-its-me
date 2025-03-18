5b9913e5608d478c8a185252bcf01703a145c0df
arty0928 <arty0928@konkuk.ac.kr>
2025-03-18 16:05:18 +0900

    FEAT: Comment - insert의 CommentRequest Class 생성

--- springboot-app/src/main/java/com/uplus/eureka/comment/model/dto/CommentRequest.java
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/dto/CommentRequest.java
@@ -1,2 +1,18 @@
-package com.uplus.eureka.comment.model.dto;public class CommentRequest {
+package com.uplus.eureka.comment.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentRequest {
+    private Integer userId;     // user_id
+    private String commentText;     // comment_text
+
+    public CommentRequest(Integer userId, String commentText) {
+        this.userId = userId;
+        this.commentText = commentText;
+    }
 }
