bf7b0fad9c33fca3c81b78f46f7247181b2a5f2b
arty0928 <arty0928@konkuk.ac.kr>
2025-03-18 10:39:23 +0900

    Update CommentDao.java

--- springboot-app/src/main/java/com/uplus/eureka/comment/model/dao/CommentDao.java
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/dao/CommentDao.java
@@ -2,8 +2,16 @@ package com.uplus.eureka.comment.model.dao;
 
 import com.uplus.eureka.comment.model.dto.Comment;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.jdbc.SQL;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Mapper
 public interface CommentDao {
-    Comment get(String commentId);
+    public List<Comment> getAllComments() throws SQLException;
+    public int totalCount() throws SQLException;
+    public Comment getCommentById(String commentId) throws SQLException;
+
+
 }
