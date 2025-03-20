644014e3848c1206341c03466521d09ae7ecccf0
arty0928 <arty0928@konkuk.ac.kr>
2025-03-20 09:12:36 +0900

    FEAT: #41 - 댓글 삭제 작성자 권한 오류 해결 - 에러문 처리 msg 만 보이기 - 에러문 status 세분화

--- springboot-app/src/main/java/com/uplus/eureka/comment/model/exception/GlobalExceptionHandler.java
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/exception/GlobalExceptionHandler.java
@@ -1,29 +1,19 @@
-//package com.uplus.eureka.comment.model.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    // CommentException 처리
-//    @ExceptionHandler(CommentException.class)
-//    public ResponseEntity<String> handleCommentException(CommentException ex) {
-//        // 콘솔에 오류 메시지 출력
-//        System.out.println("Error occurred: " + ex.getMessage());
-//
-//        // 500 상태 코드와 함께 에러 메시지 반환
-//        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//
-//    // 다른 예외 처리 (예: SQLException 등) 추가 가능
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handleException(Exception ex) {
-//        // 콘솔에 오류 메시지 출력
-//        System.out.println("Error occurred: " + ex.getMessage());
-//
-//        return new ResponseEntity<>("알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//}
+package com.uplus.eureka.comment.model.exception;
+
+import com.uplus.eureka.comment.model.exception.CommentException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CommentException.class)
+    public ResponseEntity<Map<String, Object>> handleCommentException(CommentException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("msg", ex.getMessage()); // 에러 메시지만 반환
+        return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+}
