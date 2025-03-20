e858fa552e541934b5edeb3c6ada282a043cd110
arty0928 <arty0928@konkuk.ac.kr>
2025-03-20 13:13:51 +0900

    FEAT: Comment Swagger 추가

--- springboot-app/src/main/java/com/uplus/eureka/comment/model/dto/CommentRequest.java
+++ springboot-app/src/main/java/com/uplus/eureka/comment/model/dto/CommentRequest.java
@@ -1,15 +1,19 @@
 package com.uplus.eureka.comment.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class CommentRequest implements Serializable {
+
     @JsonProperty("user_id")
+    @Schema(description = "댓글 작성자 ID", example = "1")
     private Integer userId;
 
     @JsonProperty("comment_text")
+    @Schema(description = "댓글 내용", example = "좋은 투표 결과입니다.")
     private String commentText;
 }
