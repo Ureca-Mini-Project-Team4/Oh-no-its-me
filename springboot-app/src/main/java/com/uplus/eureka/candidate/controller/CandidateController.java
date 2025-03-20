00c7d57ea8436412a3a35e2e67e13758d8e2654e
arty0928 <arty0928@konkuk.ac.kr>
2025-03-20 13:19:54 +0900

    FEAT: #50 Swagger 정리 - poll - candidate - comment

--- springboot-app/src/main/java/com/uplus/eureka/candidate/controller/CandidateController.java
+++ springboot-app/src/main/java/com/uplus/eureka/candidate/controller/CandidateController.java
@@ -3,12 +3,17 @@ package com.uplus.eureka.candidate.controller;
 import com.uplus.eureka.candidate.model.dto.Candidate;
 import com.uplus.eureka.candidate.model.service.CandidateService;
 import com.uplus.eureka.candidate.model.dto.CandidateNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@Tag(name = "Poll Management API", description = "투표 관리 API")
 @RestController
 public class CandidateController {
 
@@ -18,6 +23,7 @@ public class CandidateController {
         this.candidateService = candidateService;
     }
 
+    @Operation(summary = "투표의 모든 후보자 조회", description = "해당 투표의 모든 후보자 조회")
     @GetMapping("api/candidate/{pollId}")
     public ResponseEntity<?> getCandidates(@PathVariable("pollId") int pollId) {
         try {
--- springboot-app/src/main/java/com/uplus/eureka/poll/controller/PollController.java
+++ springboot-app/src/main/java/com/uplus/eureka/poll/controller/PollController.java
@@ -3,12 +3,17 @@ package com.uplus.eureka.poll.controller;
 import com.uplus.eureka.poll.model.dto.Question;
 import com.uplus.eureka.poll.model.service.PollService;
 import com.uplus.eureka.poll.model.dto.PollServiceException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@Tag(name = "Candidate Management API", description = "후보자 관리 API")
 @RestController
 @RequestMapping("/api")
 public class PollController {
@@ -19,6 +24,7 @@ public class PollController {
         this.pollService = pollService;
     }
 
+    @Operation(summary = "모든 투표 조회", description = "모든 투표 가져오기")
     @GetMapping("/poll")
     public ResponseEntity<List<Question>> getRandomQuestions() {
         try {
