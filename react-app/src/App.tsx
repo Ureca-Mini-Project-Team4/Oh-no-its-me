558e146193afdfae0783a2b662186eb01c268c8b
kuru2141 <kuru2141@naver.com>
2025-05-04 00:41:33 +0900

    FEAT: App 초기화

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
   const queryClient = new QueryClient();
-
+  
   return (
     <QueryClientProvider client={queryClient}>
       <FontTest />
