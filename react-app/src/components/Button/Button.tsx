e12002dca6960081246e12200069d2056c7f915b
leedaye0412 <neoldy00@naver.com>
2025-05-07 00:31:29 +0900

    FEAT: 버튼 컴포넌트 수정

--- react-app/src/components/Button/Button.tsx
+++ react-app/src/components/Button/Button.tsx
@@ -6,22 +6,45 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   disabled = false,
   type,
+  size,
   width,
   height,
+  borderRadius,
 }) => {
-  const baseStyle = 'rounded-[20px] font-bold text-center font-pr';
+  const radiusClass = size === 'sm' ? 'rounded-[8px]' : size === 'lg' ? 'rounded-[20px]' : '';
+
+  const baseStyle = `font-bold text-center font-pr ${radiusClass}`;
 
   const typeStyle =
     type === 'default'
       ? 'bg-[var(--color-primary-base)] text-white hover:bg-[var(--color-primary-hover)]'
       : 'bg-white text-[var(--color-primary-base)] border border-[var(--color-primary-base)] hover:bg-[var(--color-primary-light)]/15';
 
+  const sizeStyle = (() => {
+    switch (size) {
+      case 'lg':
+        return 'w-[188px] h-[55px] text-xl';
+      case 'sm':
+        return 'w-[57px] h-[34px] text-sm';
+      default:
+        return '';
+    }
+  })();
+
+  const inlineStyle = !size
+    ? {
+        width,
+        height,
+        borderRadius,
+      }
+    : undefined;
+
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${baseStyle} ${typeStyle}`}
-      style={{ width, height }}
+      className={`${baseStyle} ${typeStyle} ${sizeStyle}`}
+      style={inlineStyle}
     >
       {label}
     </button>
