c703d3116ec8b90f006ab0dfa18034b91e2c8396
kuru2141 <kuru2141@naver.com>
2025-05-08 11:09:46 +0900

    FEAT: border-radius 변경

--- react-app/src/components/Comment/Comment.tsx
+++ react-app/src/components/Comment/Comment.tsx
@@ -7,14 +7,14 @@ interface CommentProps {
 
 const Comment = ({ nickname, comment }: CommentProps) => {
   return (
-    <div className="w-[300px] pt-[5px] pb-[5px] h-auto sm:w-[450px] flex flex-initial rounded-[30px] border-solid border-[1px] border-gray-100 items-center">
-      <div className="flex gap-[10px] pl-3 pr-3">
+    <div className="w-[300px] pt-[5px] pb-[5px] h-auto sm:w-[450px] flex flex-initial rounded-xl sm:rounded-2xl border-solid border-[1px] border-gray-100 items-center">
+      <div className="flex gap-[10px] pl-3 pr-3 items-center">
         <div className="h-full flex items-start">
           <Profile nickname={nickname} />
         </div>
         <div className="font-pm flex flex-col gap-[5px]">
-          <div className="text-[10px] sm:text-[14px] text-gray-600 mt-[5px]">{nickname}</div>
-          <div className="text-[12px] sm:text-[16px] mb-[5px]">{comment}</div>
+          <div className="text-[8px] sm:text-[12px] text-gray-600 mt-[5px]">{nickname}</div>
+          <div className="text-[10px] sm:text-[15px] mb-[5px]">{comment}</div>
         </div>
       </div>
     </div>
--- react-app/src/components/Profile/Profile.tsx
+++ react-app/src/components/Profile/Profile.tsx
@@ -6,7 +6,7 @@ interface ProfileProps {
 
 const Profile = ({ nickname }: ProfileProps) => {
   return (
-    <div className="w-[50px] h-[50px] sm:w-[70px] sm:h-[70px] rounded-xl">
+    <div className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] rounded-xl">
       <img
         className="border-[1px] rounded-4xl border-gray-100"
         src={`${BASE_URL}${nickname ? 'animal/' + nickname + '.jpg' : 'default-profile.png'}`}
