6ce4c8a31461798850d866f6dfe8e3f2ca26b903
abyss-s <lh3729@naver.com>
2025-05-09 13:02:05 +0900

    REFACTOR: 토스트 훅을 사용하기 쉽도록 주석 추가

--- react-app/src/hook/useToast.ts
+++ react-app/src/hook/useToast.ts
@@ -1,16 +1,17 @@
 import { useDispatch } from 'react-redux';
-import { show, hide } from '@/store/slices/toastSlice';
-import { ToastType } from '@/store/slices/toastSlice';
+import { show, hide, ToastType } from '@/store/slices/toastSlice';
 
+/**
+ * 사용법
+ * - const { showToast } = useToast();
+ * - showToast('성공적으로 완료되었습니다!', 'success')
+ */
 export function useToast() {
   const dispatch = useDispatch();
 
-  const showToast = (
-    message: string,
-    type: ToastType = 'information',
-    duration: number = 4000, // 4초
-  ) => {
+  const showToast = (message: string, type: ToastType = 'information', duration: number = 4000) => {
     dispatch(show({ message, type }));
+
     setTimeout(() => {
       dispatch(hide());
     }, duration);
