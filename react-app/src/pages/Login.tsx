456f6d28cf89e357b6d08e0e3b5703682d50d3ce
abyss-s <lh3729@naver.com>
2025-05-09 16:22:00 +0900

    FEAT: #110 로그인 테스트 페이지

--- react-app/src/pages/Login.tsx
+++ react-app/src/pages/Login.tsx
@@ -18,30 +18,32 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-sm">
-        <h2 className="text-2xl font-bold text-center mb-6">로그인 테스트 페이지</h2>
+        <h2 className="font-gumi text-2xl font-bold text-center mb-6">
+          <span className="text-(--color-primary-base)">너</span>로 정했다!
+        </h2>
         <div className="mb-4">
-          <label className="block text-gray-700 mb-2">아이디</label>
+          <label className="font-pm block text-gray-700 mb-2">아이디</label>
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-(--color-primary-base)/70"
             placeholder="아이디를 입력하세요"
           />
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 mb-2">비밀번호</label>
+          <label className="font-pm block text-gray-700 mb-2">비밀번호</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-(--color-primary-base)/70"
             placeholder="비밀번호를 입력하세요"
           />
         </div>
         <button
           onClick={handleLogin}
-          className="w-full py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-200"
+          className="w-full py-2 bg-(--color-primary-base) text-white font-semibold rounded-lg hover:bg-(--color-primary-hover) transition duration-200"
         >
           로그인
         </button>
