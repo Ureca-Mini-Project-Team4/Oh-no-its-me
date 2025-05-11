8febc048fb932a1e6ecfd775033a9fc7d283b583
kuru2141 <kuru2141@naver.com>
2025-05-11 21:25:10 +0900

    REFACTOR: 비밀번호 변경 수정

--- springboot-app/src/main/java/com/uplus/eureka/user/model/service/UserService.java
+++ springboot-app/src/main/java/com/uplus/eureka/user/model/service/UserService.java
@@ -5,7 +5,7 @@ import com.uplus.eureka.user.model.dto.User;
 public interface UserService {
 	User login(String username, String pass);
 	User getUser(Integer userId);
-	void updatePassword(Integer userId, String password);
+	void updatePassword(Integer userId, String new_pass);
 	void saveRefreshToken(Integer userId, String refreshToken);
 	String getRefreshToken(Integer userId);
 	void deleteRefreshToken(Integer userId);
