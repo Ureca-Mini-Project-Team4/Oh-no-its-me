32981cb95fd905d18a2639e61b44a0280381ace1
abyss-s <lh3729@naver.com>
2025-05-12 15:41:22 +0900

    FEAT: 랭킹 페이지 타이틀 배경 렌더링 수정

--- react-app/src/pages/Result.tsx
+++ react-app/src/pages/Result.tsx
@@ -1,10 +1,13 @@
 const Result = () => {
   return (
-    <div className="w-full h-48 bg-() ">
-      <div className="flex flex-row items-center justify-center z-1">
-        <img src="assets/images/popper-left.png" alt="popper" className="lg:w-15 sm:w-11 h-auto" />
-        <h1 className="font-pb text-2xl">너로 정했다!</h1>
-        <img src="assets/images/popper-right.png" alt="popper" className="lg:w-15 sm:w-11 h-auto" />
+    <div className="relative w-full h-screen bg-responsive">
+      {/* 내용 */}
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center z-10">
+        <div className="flex flex-row items-center justify-center mb-6">
+          <img src="/assets/images/popper-left.png" alt="popper" className="w-10 sm:w-15 h-auto" />
+          <h1 className="font-pb text-2xl text-black mx-4">너로 정했다!</h1>
+          <img src="/assets/images/popper-right.png" alt="popper" className="w-10 sm:w-15 h-auto" />
+        </div>
       </div>
     </div>
   );
