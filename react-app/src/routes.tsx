3227bd991581cf896d713d7cc2b2b6e96c73f97d
abyss-s <lh3729@naver.com>
2025-05-12 13:39:43 +0900

    FEAT: 라우팅 파일에 모든 페이지 경로 추가
    - 404 에러 페이지 추가

--- react-app/src/routes.tsx
+++ react-app/src/routes.tsx
@@ -4,10 +4,13 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Vote from './pages/Vote';
 import ChangePW from './pages/ChangePW';
+import Result from './pages/Result';
+import Main from './pages/Main';
+import Comment from './pages/Comment';
+import ErrorBoundary from './pages/ErrorBoundary';
 
 const router = createBrowserRouter([
   {
-    // path: '/',
     element: <Layout />,
     children: [
       {
@@ -25,6 +28,22 @@ const router = createBrowserRouter([
       {
         path: 'change-password',
         element: <ChangePW />,
+      },
+      {
+        path: '/result',
+        element: <Result />,
+      },
+      {
+        path: '/main',
+        element: <Main />,
+      },
+      {
+        path: '/comment',
+        element: <Comment />,
+      },
+      {
+        path: '*',
+        element: <ErrorBoundary />,
       },
     ],
   },
