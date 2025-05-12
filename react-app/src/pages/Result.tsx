5a70fec9c2293497c000dc9b27b87839213b1a9c
abyss-s <lh3729@naver.com>
2025-05-12 13:36:27 +0900

    ADD: 랭킹 페이지를 위한 배경 추가

--- react-app/src/pages/Result.tsx
+++ react-app/src/pages/Result.tsx
@@ -1,5 +1,13 @@
 const Result = () => {
-  return <div></div>;
+  return (
+    <div className="w-full h-48 bg-() ">
+      <div className="flex flex-row items-center justify-center z-1">
+        <img src="assets/images/popper-left.png" alt="popper" className="lg:w-15 sm:w-11 h-auto" />
+        <h1 className="font-pb text-2xl">너로 정했다!</h1>
+        <img src="assets/images/popper-right.png" alt="popper" className="lg:w-15 sm:w-11 h-auto" />
+      </div>
+    </div>
+  );
 };
 
 export default Result;
