d1d74a9f65447d688f83b66a022851068841bbb4
leedaye0412 <neoldy00@naver.com>
2025-05-13 15:17:51 +0900

    FEAT: vote ui

--- react-app/src/pages/Vote.tsx
+++ react-app/src/pages/Vote.tsx
@@ -13,7 +13,7 @@ const Vote = () => {
   };
 
   const handleNext = () => {
-    setPage((prev) => prev + 1);
+    setPage((prev) => Math.min(prev + 1, 4));
   };
 
   return (
@@ -42,19 +42,19 @@ const Vote = () => {
           </div>
           <div className="flex justify-between items-center w-full p-5">
             <Button label="이전" onClick={handlePrev} type="outline" size="sm"></Button>
-            <Button label="이후" onClick={handleNext} size="sm"></Button>
+            <Button label="다음" onClick={handleNext} size="sm"></Button>
           </div>
         </div>
       ) : (
         <div className="p-5">
           <div className="flex flex-col bg-gray-50 p-5 rounded-[30px] sm:flex-row">
-            <div className="p-20">
-              <div className="flex flex-col items-center justify-center gap-10 font-ps text-sm text-center sm:text-3xl">
+            <div className="flex flex-1 p-10">
+              <div className="flex flex-col items-center justify-center gap-10 font-ps text-2xl text-center ">
                 <p>무인도에서 가장 탈출 못할 것 같은 사람은?</p>
                 <img src="/public/assets/images/question/island.svg" />
               </div>
             </div>
-            <div className="flex flex-col items-center justify-center gap-10">
+            <div className="flex flex-1 flex-col items-center justify-center gap-5">
               <div className="flex flex-row">
                 <Candidate name="이영주"></Candidate>
                 <Candidate name="박은서"></Candidate>
@@ -65,9 +65,9 @@ const Vote = () => {
               </div>
             </div>
           </div>
-          <div className="flex justify-between items-center w-full p-10">
+          <div className="flex justify-between items-center w-full mt-10">
             <Button label="이전" onClick={handlePrev} type="outline" size="lg"></Button>
-            <Button label="이후" onClick={handleNext} size="lg"></Button>
+            <Button label="다음" onClick={handleNext} size="lg"></Button>
           </div>
         </div>
       )}
