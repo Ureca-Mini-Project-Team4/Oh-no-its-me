fa7017a3ffedb1d1c64aea09d2544248dcdaf38e
kuru2141 <kuru2141@naver.com>
2025-05-13 15:00:53 +0900

    FEAT: candidateGroup 추가

---
+++ react-app/src/apis/candidate/getCandidateLatest.ts
@@ -0,0 +1,13 @@
+import axiosInstance from '../axiosInstance';
+
+export interface getCandidateLatestResponse {
+  pollId: number;
+  userName: string;
+  questionText: string;
+  //   icon: string;
+}
+
+export async function getCandidateLatests(): Promise<getCandidateLatestResponse[]> {
+  const response = await axiosInstance.get('/candidate/latest');
+  return response.data;
+}
