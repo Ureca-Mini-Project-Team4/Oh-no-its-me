5155bdee237da24e1282a4ba2b83a19c318c814e
abyss-s <lh3729@naver.com>
2025-05-18 22:45:23 +0900

    REFACTOR: #216 모달 컴포넌트 디자인 리팩토링

--- react-app/src/components/Modal/Modal.tsx
+++ react-app/src/components/Modal/Modal.tsx
@@ -15,12 +15,12 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
-      <div className="bg-white rounded-2xl shadow-lg px-6 py-8 w-[90%] max-w-sm text-center">
-        <p className="font-pr text-lg sm:text-xl font-semibold text-gray-900 mb-3">{text1}</p>
-        {text2 && <p className="font-pr text-sm sm:text-base text-gray-600 mb-6">{text2}</p>}
-        <div className="flex justify-center gap-3">
-          <Button label="취소" type="outline" onClick={handleCancel} />
-          <Button label="확인" onClick={onConfirm} />
+      <div className="bg-white rounded-2xl shadow-lg px-12 py-8 w-[90%] max-w-sm text-center">
+        <p className="font-pr text-[18px]  text-gray-900 mb-3">{text1}</p>
+        {text2 && <p className="font-pr text-base text-gray-600 mb-6">{text2}</p>}
+        <div className="flex justify-center gap-3 mt-12">
+          <Button label="취소" type="outline" onClick={handleCancel} size="full" />
+          <Button label="확인" onClick={onConfirm} size="full" />
         </div>
       </div>
     </div>
