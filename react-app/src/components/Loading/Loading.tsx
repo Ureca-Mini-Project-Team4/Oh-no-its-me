55dc2c2bd4ff30168e84c3e1793b46c336779895
abyss-s <lh3729@naver.com>
2025-05-19 11:24:51 +0900

    REFACTOR: #228 로그인 로딩 추가

--- react-app/src/components/Loading/Loading.tsx
+++ react-app/src/components/Loading/Loading.tsx
@@ -2,8 +2,8 @@ import { ICONS } from '@/constants/iconPath';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 z-[999] flex flex-col items-center justify-center bg-white">
-      <img src={ICONS.SPINNER} alt="로딩중" className="w-16 h-16 sm:w-24 sm:h-24" />
+    <div className="w-full min-h-[150px] z-[999] flex flex-col items-center justify-center">
+      <img src={ICONS.SPINNER} alt="로딩중" className="w-12 h-12 sm:w-16 sm:h-16" />
     </div>
   );
 };
