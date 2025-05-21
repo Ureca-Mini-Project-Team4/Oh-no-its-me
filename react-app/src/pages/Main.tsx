618d7908f61c36d4b456430034882bd24ab9b385
kuru2141 <kuru2141@naver.com>
2025-05-21 11:48:32 +0900

    REFACT: Poll candidate 수정

--- react-app/src/pages/Main.tsx
+++ react-app/src/pages/Main.tsx
@@ -1,15 +1,28 @@
 import Button from '@/components/Button/Button';
 import FloatingBackground from '@/components/FloatingBackground/FloatingBackground';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { formatMillisecondsToHMS, getTodayAtHour } from '@/utils/time';
 
 const Main = () => {
   const [restTime, setRestTime] = useState(10);
+
+  const nowTime = useRef(Date.now());
+  const eightTime = new Date();
+  eightTime.setHours(8, 0, 0, 0);
+
   const isVoted = localStorage.getItem('voted') === 'true';
 
+  const changeDateTime = useCallback((diff: number) => {
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }, []);
+
   useEffect(() => {
-    const todayAtFour = getTodayAtHour(16); // 16시
+    const todayAtFour = new Date();
+    todayAtFour.setHours(16, 0, 0, 0); // 16시
 
     const updateTimer = () => {
       const now = new Date().getTime();
@@ -39,30 +52,38 @@ const Main = () => {
       <FloatingBackground />
       <div className="flex flex-col justify-center gap-6">
         <p className="text-base sm:text-lg md:text-xl text-gray-600 font-pb">
-          {restTime > 0 ? '투표 결과 발표까지 남은 시간' : '투표 집계가 완료되었습니다.'}
+          {nowTime.current > eightTime.getTime() && restTime > 0
+            ? '투표 결과 발표까지 남은 시간'
+            : '투표 집계가 완료되었습니다.'}
         </p>
         <p
           className={`${
-            restTime > 0
+            nowTime.current > eightTime.getTime() && restTime > 0
               ? 'text-gray-600'
               : 'bg-gradient-to-r from-red-500 to-lime-300 bg-clip-text text-transparent'
           } text-3xl sm:text-4xl md:text-5xl font-pb`}
         >
-          {restTime > 0 ? formatMillisecondsToHMS(restTime) : '결과 발표'}
+          {nowTime.current > eightTime.getTime() && restTime > 0
+            ? changeDateTime(restTime)
+            : '결과 발표'}
         </p>
         <p className="mt-4 text-3xl sm:text-5xl md:text-6xl font-pb leading-none">
           <span className="bg-[var(--color-primary-base)] px-2">너로</span> 정했다!
         </p>
       </div>
       <div className="w-full max-w-xs text-base sm:text-lg md:text-xl font-pb mt-10">
-        {restTime > 0 ? (
+        {nowTime.current > eightTime.getTime() && restTime > 0 ? (
           <Button
             onClick={handleMoveToVote}
             label={isVoted ? '투표완료' : '투표하기'}
             disabled={isVoted}
           />
         ) : (
-          <Button onClick={handleMoveToResult} label="결과 확인" />
+          <Button
+            onClick={handleMoveToResult}
+            label="결과 확인"
+            disabled={nowTime.current - eightTime.getTime() > 0 ? true : false}
+          />
         )}
       </div>
     </div>
